6c86b50bbac1a492f1bc2d2a9153ae3eae204d01
Alexander Pietsch <alexander.pietsch@googlemail.com>
2019-01-17 21:28:25 +0100

    minor stuff

--- src/main/java/de/utrl/decoratorPattern/ChristmasTreeImpl.java
+++ src/main/java/de/utrl/decoratorPattern/ChristmasTreeImpl.java
@@ -1,6 +1,6 @@
 package de.utrl.decoratorPattern;
 
-public class ChristmasTreeImpl implements ChristmasTree{
+public class ChristmasTreeImpl implements ChristmasTree {
     public String decorate() {
         return "Christmas tree";
     }
--- src/test/java/de/utrl/decoratorPattern/ChristmasTreeDecoratorTest.java
+++ src/test/java/de/utrl/decoratorPattern/ChristmasTreeDecoratorTest.java
@@ -1,15 +1,13 @@
 package de.utrl.decoratorPattern;
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ChristmasTreeDecoratorTest {
 
     @Test
-    public void testDecorator() {
+    public void testGarlandDecorator() {
         ChristmasTree christmasTree = new ChristmasTreeImpl();
         assertEquals("Christmas tree", christmasTree.decorate());
         ChristmasTree garlandedTree = new Garland(christmasTree);
